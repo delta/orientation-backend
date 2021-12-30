014a47c06f7de6f67bf1a611216bcc57686062b9
kelpikz <ajithaprasad96@gmail.com>
2021-12-30 12:59:14 +0530

    feat: add route to get singleMapData

--- core/api.go
+++ core/api.go
@@ -13,6 +13,7 @@ func RegisterRoutes(v *echo.Group) {
 	v.GET("/user/me", GetUserData)
 	v.PUT("/user/signup", UpdateUserData)
 	v.GET("/user/map", getUserMap)
+	v.GET("/user/map/:userId", getSingleUserMap)
 }
 
 // returns the user data with the given credentials
@@ -93,4 +94,30 @@ func getUserMap(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusAccepted, getUserMapResponse{UserMap: userMap, Success: true})
+}
+
+func getSingleUserMap(c echo.Context) error {
+	l := logger.WithFields(logger.Fields{"method": "core/getSingleUserMap", "userId": c.Param("userId")})
+
+	db := config.DB
+	var userMap userData
+
+	userId := c.Param("userId")
+
+	l.Infof("A user requested for UserMap for a user with userId=%s", userId)
+
+	if userId == "" {
+		l.Infof("User with the given userId doesn't exist in db")
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Message: "Invalid userId"})
+	}
+
+	if err := db.Table("User").Select("id", "name", "spriteType").Where("id = ?", userId).First(&userMap).Error; err != nil {
+		l.Errorf("Erorr %e fetching user map from db", err)
+		return c.JSON(http.StatusInternalServerError, getSingleUserMapResponse{UserMap: userMap, Success: false})
+	}
+
+	l.Debugf("Found the data, userMap=%v ", userMap)
+	l.Infof("Found the user details")
+
+	return c.JSON(http.StatusOK, getSingleUserMapResponse{UserMap: userMap, Success: true})
 }
