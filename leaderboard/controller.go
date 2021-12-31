8d3679b56262eb4713185110ef3bd8ff72bea66d
Ajitha Prasad Y <59395177+kelpikz@users.noreply.github.com>
2021-12-31 17:07:28 +0530

    Feat: add route to fetch leaderboard (#15)
    * Feat: add route to fetch leaderboard
    
    * feat: add the leaderboard route to main app

--- leaderboard/controller.go
+++ leaderboard/controller.go
@@ -1,6 +1,8 @@
 package leaderboard
 
 import (
+	"net/http"
+
 	"github.com/delta/orientation-backend/auth"
 	"github.com/delta/orientation-backend/config"
 	"github.com/delta/orientation-backend/models"
@@ -44,4 +46,37 @@ func handleAddScore(c echo.Context, game_name string, score int) error {
 		}
 	}
 	return nil
+}
+
+func getLeaderBoard(c echo.Context) error {
+	l := logger.WithFields(logger.Fields{
+		"method": "leaderboard/routes/getLeaderBoard",
+		"name":   c.Param("minigameName"),
+	})
+
+	l.Infoln("getleaderBoard requested")
+
+	minigameName := c.Param("minigameName")
+
+	var response []leaderboard
+
+	db := config.DB
+
+	var miniGame models.MiniGame
+
+	if err := db.Where("name = ?", minigameName).First(&miniGame).Error; err != nil {
+		l.Errorf("Error fetching miniGame from db %+v", err)
+		return c.JSON(http.StatusBadRequest, leaderBoardResponse{Leaderboard: response, Message: "invalid minigame"})
+	}
+
+	query := "SELECT u.userName AS name, L.score AS score, u.department AS department, u.id FROM LeaderBoard AS L LEFT JOIN User AS u ON L.userId = u.id WHERE L.miniGameId = ? ORDER BY L.score DESC;"
+
+	if err := db.Raw(query, miniGame.ID).Scan(&response).Error; err != nil {
+		l.Errorf("Error fetching leaderboard from db %+v", err)
+		return c.JSON(http.StatusInternalServerError, leaderBoardResponse{Leaderboard: response, Message: "internal server error"})
+	}
+
+	l.Infof("getleaderBoard for minigame id %s is successful", minigameName)
+
+	return c.JSON(http.StatusOK, leaderBoardResponse{Leaderboard: response, Message: "success"})
 }
--- leaderboard/responses.go
+++ leaderboard/responses.go
@@ -4,3 +4,15 @@ type ScoreAddStatus struct {
 	Status  bool   `json:"status"`
 	Message string `json:"message"`
 }
+
+type leaderBoardResponse struct {
+	Leaderboard []leaderboard `json:"leaderboard"`
+	Message     string        `json:"message"`
+}
+
+type leaderboard struct {
+	UserId     int    `json:"userId"`
+	Name       string `json:"name"`
+	Department string `json:"department"`
+	Score      int    `json:"score"`
+}
